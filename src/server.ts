aa6a0e58af0c4fc1208f2e0b7e65b73e5e911ec0
Vinicios Engelage <viniciosvalensuela@gmail.com>
2022-05-08 19:23:28 -0300

    Add docker

--- src/server.ts
+++ src/server.ts
@@ -4,4 +4,4 @@ const app = express();
 
 app.get("/", (request, response) => response.json({ message: "Hello world" }));
 
-app.listen(3333, () => console.log("Sever is now running!"));
+app.listen(3333, () => console.log("Sever is now running by docker!"));
