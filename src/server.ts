22a6d230956b92bc8ec78d69944dea4912798be3
Vinicios Engelage <viniciosvalensuela@gmail.com>
2022-05-08 19:18:17 -0300

    First commit

---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (request, response) => response.json({ message: "Hello world" }));
+
+app.listen(3333, () => console.log("Sever is now running!"));
