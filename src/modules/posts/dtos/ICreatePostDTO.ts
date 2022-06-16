a9f8de011d8969f5bf274d642c8b57911c6df32e
Vinicios Engelage <viniciosvalensuela@gmail.com>
2022-06-16 10:14:33 -0300

    :recycle: Change create post dto

---
+++ src/modules/posts/dtos/ICreatePostDTO.ts
@@ -0,0 +1,11 @@
+class ICreatePostDTO {
+  id?: string;
+  title: string;
+  description?: string;
+  content: string;
+  thumbnail?: string;
+  see_more_url?: string;
+  user_id: string;
+}
+
+export { ICreatePostDTO };
