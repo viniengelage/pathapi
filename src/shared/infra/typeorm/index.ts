171cf78484b4096db86704da1ac42cdf235b543d
Vinicios Engelage <viniciosvalensuela@gmail.com>
2022-06-23 18:25:51 -0300

    Add db configs

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,12 +1,16 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-export default async (host = process.env.DB_HOST): Promise<Connection> => {
+export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host: process.env.NODE_ENV === "development" ? host : process.env.DB_HOST,
-      database: defaultOptions.database,
+      type: "postgres",
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
     })
   );
 };
