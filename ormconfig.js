35b9694405a29ec7a27e7dc0dfb3b7a3fbcca64a
Vinicios Engelage <viniciosvalensuela@gmail.com>
2022-06-23 20:21:11 -0300

    config orm module exports

--- ormconfig.js
+++ ormconfig.js
@@ -1,4 +1,4 @@
-module.exports = {
+const devConfig = {
   type: "postgres",
   url: process.env.DATABASE_URL,
   host: process.env.DB_HOST,
@@ -16,4 +16,26 @@ module.exports = {
   cli: {
     migrationsDir: "./src/shared/infra/typeorm/migrations",
   },
-};
+}
+
+const productionConfig = {
+  type: "postgres",
+  url: process.env.DATABASE_URL,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false
+    },
+  },
+  migrations: ["./dist/shared/infra/typeorm/migrations/*.js"],
+  entities: ["./dist/modules/**/entities/*.js"],
+  cli: {
+    migrationsDir: "./dist/shared/infra/typeorm/migrations",
+  },
+}
+
+module.exports = process.env.NODE_ENV = 'production' ? productionConfig : devConfig;
