6456433048728c4c99f5e1db9fb2b80cafec9260
Vinicios Engelage <viniciosvalensuela@gmail.com>
2022-06-23 19:40:24 -0300

    add ssl to typeorm

--- ormconfig.js
+++ ormconfig.js
@@ -5,6 +5,9 @@ module.exports = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  extra: {
+    ssl: process.env.NODE_ENV === 'production' ? true : false,
+  },
   migrations: ["./src/shared/infra/typeorm/migrations/*.ts"],
   entities: ["./src/modules/**/entities/*.ts"],
   cli: {
