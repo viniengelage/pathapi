96d255e0478240820e2852de19f53e7fd8018658
Vinicios Engelage <viniciosvalensuela@gmail.com>
2022-06-26 15:07:42 -0300

    Test error

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -45,6 +45,7 @@ app.use(router);
 
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
+    console.log(err);
     if (err instanceof AppError) {
       return response.status(err.statusCode).json({
         status: "error",
